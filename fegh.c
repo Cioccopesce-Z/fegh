dbfb1dcfb745e27282d4f968611c663caebf001d
MrOld <cioccopesce43@gmail.com>
2026-07-26 00:31:08 +0200

    added shift with carry

--- fegh.c
+++ fegh.c
@@ -6,30 +6,34 @@
 
 #define to_declare 20
 #define byte_for_dim 2
-#define byte_for_scope 2
+#define byte_for_scope 0
 #define byte_for_method_lenght 2
 #define byte_for_vleng 1
 
 #define tru 1
 #define fal 0
 #define auto fal
+#define up 16
+#define down 4
 
 #define global_scope 0
 #define main_scope 1
 
-
 __uint8_t *memory;
+
+size_t memory_cursor = 0;
 
 //0 code_scope    <---- address pt1
 //1 code_scope    <---- address pt2
-//2 var dim     
-//3 var dim     
+
+//2 var dim       <---- sum eq address end line (eg 10)
+//3 var dim       <---- sum eq address end line (eg 10)
 //4 lenght in byte
 //5 value
 //6 value
 //7 method_lenght in byte
+//8 value
 //9 value
-//10 value
 
 
 
@@ -40,13 +44,13 @@ void begin(size_t byte_to_allocate){
 
 }
 
-
-
 size_t bytes_needed(__uintmax_t value){
     size_t n = 1;
     while (value >> (8 * n)) n++;
     return n;
 }
+
+// FUNCTION TO READ
 
 size_t get_lenght_in_byte_from_address(size_t address){
 
@@ -66,16 +70,72 @@ size_t get_current_scope_by_address(size_t address){
 
 }
 
+size_t get_value_of_variable(size_t address){
+
+    __uint8_t lenght_of_value = memory[address + byte_for_scope +byte_for_dim];
+
+    size_t value = 0;
+
+    for (int i = 1; i <= lenght_of_value; i++) {
+        value |= (size_t)memory[address + byte_for_scope + byte_for_dim + i] << (8 * (i-1));
+    }
+
+    return value;
+}
+
+//FUNCTION TO WRITE
+
+void shift_all_memory(int direction, int number_of_shifts, int use_carry,size_t memory_lenght_in_byte){
+
+    size_t i = 0;
+
+    if(direction == up){
+
+        __uint8_t carry = 0;
+
+
+        for(i = 0; i < number_of_shifts; i++)
+            for(i = 0; i < memory_lenght_in_byte; i++){
+
+                memory_cursor -= number_of_shifts;
+
+                if(use_carry){
+                    carry = memory[0];
+                    memory[memory_cursor-i] = carry; 
+                }
+
+                memory[i] = memory[i+1];
+
+            }
+        
+    }
+
+    else if(direction == down){
+        for(i = 0; i < number_of_shifts; i++)
+            for(i = 0; i < memory_lenght_in_byte; i++)
+                if(i-1 == -1) memory[i] = memory[i-1];
+                    else memory[i] = memory[i-1];
+            
+    }
+
+    
+}
+
+void add_method_to_variable(){
+
+}
+
+
 /*con l'utilizzo di address si possono andare a modificare i valori dei metadati di una
 variabile nello specifico altrimenti con address = fal si incoda la variabile da dichiarare
 al primo indirizzo disponibile ritornato dalla funzione stessa
 ....................................................................
 se lenght_in_byte è = 0 allora viene assegnata autonomamente la dimensione
 mentre viene specificato con use_method se ci sono da aggiungere metodi alla variabile*/
-size_t initialize_variable(int use_address, size_t address_offset, __uintmax_t value, 
-                            size_t lenght_in_byte,
-                            int use_method, size_t method_address,
-                            int use_scope, size_t scope_address){
+size_t initialize_variable(int use_scope, size_t scope_address,
+                            int use_address, size_t address_offset,  
+                            size_t lenght_in_byte, __uintmax_t value,
+                            int use_method, size_t method_address){
 
     size_t start = 0;                            
     static size_t end = 0;
@@ -114,7 +174,7 @@ size_t initialize_variable(int use_address, size_t address_offset, __uintmax_t v
             return start;
         }
     }
-    printf("%lu %lu numero di celle value da utilizzare\n",lenght_in_byte,value);
+    printf("%lubyte for \"%lu\" numero di celle value da utilizzare\n",lenght_in_byte,value);
 
 
     if(start + byte_for_scope + byte_for_dim + lenght_in_byte >= to_declare){
@@ -150,9 +210,9 @@ size_t initialize_variable(int use_address, size_t address_offset, __uintmax_t v
     //se non ci sono metodi assegna il valore di dim e poi ritorna l'indirizzo di start
     if(!use_method){
 
-        printf("end_of_data %ld \n",end);
         if(use_address) end = backup_end;
-        printf("%lu continue from\n\n",end);
+        memory_cursor = end;
+        printf("line to %lu continue from\n\n",end);
     }
 
     //vanno aggiunti i method
@@ -163,7 +223,7 @@ size_t initialize_variable(int use_address, size_t address_offset, __uintmax_t v
             printf("error: the address of the method (%lu) is to big to be saved in %d \n", 
                         method_lenght,byte_for_method_lenght);
         }
-        printf("%lu numero di byte richiesti per salvare il codice del metodo \n", method_lenght);
+        printf("%lubyte richiesti per salvare il codice del metodo \n", method_lenght);
 
         for(int i = 0; i < byte_for_method_lenght; i++){
             memory[start + byte_for_scope + byte_for_dim + byte_for_vleng + lenght_in_byte + i]   
@@ -179,9 +239,9 @@ size_t initialize_variable(int use_address, size_t address_offset, __uintmax_t v
 
         //aggiorno i end con la lunghezza dei metodi
         end += method_lenght + byte_for_method_lenght;
-        printf("end_of_data %ld \n",end);
         if(use_address) end = backup_end;
-        printf("%lu continue from\n\n",end);
+        memory_cursor = end;
+        printf("%lu continue from address\n\n",end);
         
 
     }
@@ -195,62 +255,34 @@ size_t initialize_variable(int use_address, size_t address_offset, __uintmax_t v
    
 }
 
-size_t read_value_of_variable(size_t address){
-
-    __uint8_t lenght_of_value = memory[address + byte_for_scope +byte_for_dim];
-
-    size_t value = 0;
-
-    for (int i = 1; i <= lenght_of_value; i++) {
-        value |= (size_t)memory[address + byte_for_scope + byte_for_dim + i] << (8 * (i-1));
-    }
-
-    return value;
+int starts_with(const char *str, const char *prefix) {
+    return strncmp(str, prefix, strlen(prefix)) == 0;
 }
+
 
 int main(){
     
     begin(to_declare);
 
-
-
     printf("%p\n",memory);
     printf("\n");
-    /*
     
-    initialize_variable(fal,0,256,0,fal,0,tru,main_scope);
-    initialize_variable(fal,0,612,1,fal,0,tru,main_scope);
-    initialize_variable(fal,0,44,0,fal,0,tru,main_scope);
-    initialize_variable(fal,0,5,0,fal,0,tru,main_scope);
-    initialize_variable(fal,0,81,0,fal,0,tru,main_scope);
-    initialize_variable(fal,0,6,0,fal,0,tru,main_scope);
-
-    */
-    initialize_variable(fal,auto,4,auto,tru,256,tru,main_scope);
-    initialize_variable(fal,auto,96,auto,tru,512,tru,main_scope);
-    
+    initialize_variable(tru,main_scope,fal,auto,auto,4,tru,256);
         
+    printf("\n");
+    printf("crs position; <%lu>\n",memory_cursor);
     printf("\n");
 
     for (int i = 0; i < to_declare; i++){        
         printf("line(%d) %ld: %u\n",i, (long)(memory + i), memory[i]);
     }
-    
-    //IL CODICE DEI METODI O ADDRESS FA RIFERIMENTO AD UNA LISTA CREATA A COMPILE TIME
-    //DI METODI GIA SCRITTI, AD ESEMPIO SE L'ADDRESS FA RIDERIMENTO ALLA LISTA 210 E LA LISTA 
-    //210 CONTIENE APPEND, DOUBLE, SQUARE ECC... QUESTI METODI POTRANNO ESSERE APPLICATI 
-    //ALLA VARIABILE
-    
-    //PER LA GESTIONE DEGLI ARRAY SI USA LA STESSA STRUTTURA NELLA QUALE VENGONO SCRITTI I VALUE
-    //IN MODO SPECIALE OVVERO CHE SI SCEGLIE UNA DIMENSIONE PER IL VETTORE IN TERMINI DI CELLE
-    //E POI DI DIMENSIONI CELLA: LET ARRAY: 5(NUMERO DI CELLE):2 CHE CORRISPONDE A INT ARRAY[5]
-    //POICHE DI 2BYTE E 5 CELLE. QUI SEMPLICEMENTE VIENE CREATA UNA STRUTTURA VAR LA CUI
-    //LENGHT È UGUALE A NUMB_CELL*DIM IN MODO CHE LE 10 CELLE VALUE SEMBRINO RAGGRUPPATE IN GRUPPI
-    //DA DUE PER FORMARE 5 CELLE DELL'ARRAY QUINDI NELLA TABELLA DEI SIMBOLI SI HA:
-    // PER LE VAR:
-    //               NOME : IDX
-    // PER I VETT:
-    //               NOME : NUM_OF_CELL : DIM IN BYTE DELLE CELLE
-    //
-    //NONOSTANDE VENGANO LETTI IN MODI DIVERSI SI HA LA STESSA STRUTTURA ALLA BASE
+
+    printf("\n\n");
+    shift_all_memory(up,1,tru,to_declare);
+
+
+    for (int i = 0; i < to_declare; i++){        
+        printf("line(%d) %ld: %u\n",i, (long)(memory + i), memory[i]);
+    }
+
 }
